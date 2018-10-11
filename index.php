fae2b4f822c9f787c5475fa4637cfea9648d96a7
Teo Valean <valean_teodora16@yahoo.com>
2018-10-11 17:15:07 +0300

    Index refactorizat

--- index.php
+++ index.php
@@ -1,72 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Acasa</title>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdni.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-  <meta name="description" content="Atestat">
-    <meta name="author" content="Olivia&Teodora">
-	<link rel="icon" href="logofinal.png">
-	<link rel="stylesheet" type="text/css" href="id.css"> 
-	<link rel="stylesheet" type="text/css" href="cursor.css"> 
-  <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template 
-    <link href="sticky-footer.css" rel="stylesheet">-->
-	<link rel="stylesheet" type="text/css" href="sticky-footer.css">
-
-  
-  <style>
-  .carousel-inner > .item > img,
-  .carousel-inner > .item > a > img {
-      width: 100%;
-      margin: auto;
-  }
-
-  * {
-    padding: 0;
-    margin: 0;
-  }
-  .fit { /* set relative picture size */
-    max-width: 20%;
-    max-height: 0%;
-  }
-  .center {
-    display: block;
-    margin: auto;
-  }
-  </style>
-</head>
+<?php require 'head.php'; ?>
 <body>
+<style>
+    .carousel-inner > .item > img,
+    .carousel-inner > .item > a > img {
+        width: 100%;
+        margin: auto;
+    }
 
-<nav class="navbar navbar-inverse" style="background-color: #720031;">
-  <div class="container-fluid">
-    <ul class="nav navbar-nav">
-      <li class="#"><a href="Acasa.html">Acasă</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" style="background-color: #720031" href="#" >Articole <span class="caret"></span></a>
-        <ul class="dropdown-menu" >
-          <li><a href="Bumbisori.html">Bumbişori</a></li>
-          <li><a href="Lalele.html">Lalele</a></li>
-          <li><a href="Panselute.html">Panseluţe</a></li>
-        </ul>
-      </li>
-	  <li><a href="Galerie.html">Galerie foto</a></li>
-      <li><a href="Timeline/timeline.html">Despre Noi</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="/Timeline/index.php"><span class="glyphicon glyphicon-user"></span> Contul Meu</a></li>
-    </ul>
-  </div>
-</nav>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    .fit { /* set relative picture size */
+        max-width: 20%;
+        max-height: 0%;
+    }
+    .center {
+        display: block;
+        margin: auto;
+    }
+</style>
+
+<?php require_once 'menu.php'; ?>
+
 <!-- Begin page content -->
     <div class="container">
 	
@@ -75,22 +33,11 @@
 	
       </div>
 	<img class="center fit" SRC="d.png" STYLE="position:relative; TOP:-30px; BOTTOM:-10px; LEFT:40%;">
-	<!--<script src="https://code.jquery.com/jquery-latest.js"></script>
-	<script type="text/javascript" language="JavaScript">
-	function set_body_height() { // set body height = window height
-		$('body').height($(window).height());
-	}
-		$(document).ready(function() {
-		$(window).bind('resize', set_body_height);
-		set_body_height();
-	});
-	</script>-->
     </div>
 	
 
 
 
-		<!--<IMG STYLE="position:relative; TOP:10%; LEFT:70%; WIDTH:200px; HEIGHT:110px" SRC="d.png"></div> -->
 <div class="container">
   
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -163,15 +110,4 @@
   </div>
 </div>
 
-<footer class="footer">
-      <div class="container">
-        <p class="text-muted">Copyright © 2016 Olivia&Teodora. All rights reserved.</p>
-      </div>
- </footer>
- 
-   
-   <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-
-</body>
-</html>
+<?php require_once 'footer.php'; ?>
